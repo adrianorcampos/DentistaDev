b993e3dec557f5af7101573000250fb5edd10362
adrianorcampos <adrianorcampos@gmail.com>
2020-06-21 11:20:21 -0300

    foi

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,9 +7,9 @@ const notFound = () => {
   return (
     <Layout>
       <Helmet>
-        <title>Page not found</title>
+        <title>Página não encontrada</title>
       </Helmet>
-      <div style={{textAlign: "center", padding:"5vh 0", lineHeight: "1.5"}}>
+      <div style={{ textAlign: "center", padding: "5vh 0", lineHeight: "1.5" }}>
         <svg
           width="256"
           height="256"
@@ -37,10 +37,12 @@ const notFound = () => {
             fill="currentColor"
           />
         </svg>
-        <p>Either you are in a wrong page or you have lost. Lets go back <Link to="/">home</Link> safely</p>
+        <p>
+          Ou você digitou algo errado ou está perdido. Vamos voltar para a <Link to="/">página principal</Link>...
+        </p>
       </div>
     </Layout>
-  )
+  );
 }
 
 export default notFound
